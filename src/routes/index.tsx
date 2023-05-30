0c7cefb62edc9404711c9df5b95abbbb843b9c3d
mrhoodz <hoodzt7@gmail.com>
2023-05-30 13:01:21 +0200

    pushed it

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -21,7 +21,7 @@ export const head: DocumentHead = () => {
       { name: "xfd", content: "dsadasdasd" },
       {
         property: "og:image",
-        content: "https://satori-attempt-p3lo.vercel.app",
+        content: "https://satori-attempt-p3lo.vercel.app/sat",
       },
     ],
   };
